9f1d2ccaf645df2acab5627cfe1e869c0331e7bc
Rojas Marcos <rojasmarcosadrian.05@gmail.com>
2022-10-23 23:36:47 -0300

    prueba

--- src/main/java/com/porfolio/PRM/Security/Controller/AuthController.java
+++ src/main/java/com/porfolio/PRM/Security/Controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("auth/")
-@CrossOrigin(origins = "")
+@CrossOrigin
 public class AuthController {
     @Autowired PasswordEncoder passwordEncoder;
     @Autowired AuthenticationManager authenticationManager;
